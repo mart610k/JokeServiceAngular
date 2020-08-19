e57c34cdb4b206c51fd7b37b0bf20a59ec8a946f
mart610k <martin-baage@hotmail.com>
2020-08-19 11:57:57 +0200

    Added Abstract joke

---
+++ src/app/class/abstract-joke.spec.ts
@@ -0,0 +1,7 @@
+import { AbstractJoke } from './abstract-joke';
+
+describe('AbstractJoke', () => {
+  it('should create an instance', () => {
+    expect(new AbstractJoke()).toBeTruthy();
+  });
+});
